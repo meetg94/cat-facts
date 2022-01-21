29405d0eda91ebd53c4eec3f99a8e0455bdc189d
bakedegg <meetguleria94@gmail.com>
2022-01-20 22:01:18 -0800

    2

--- src/Components/FactCard.js
+++ src/Components/FactCard.js
@@ -1,22 +1,27 @@
 import * as React from 'react';
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import axios from "axios"
 import './FactCard.css'
 
 export default function MediaCard() {
 
     const [dog, setDog] = useState([])
 
-    React.useEffect(() => {
-        fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1')
-            .then((data) => data.json())
-            .then((data) => console.log(data.fact))
-    }, [])
+    useEffect(() => {
+        axios.get('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1', {
+            headers: {
+            }
+        })
+            .then(res => {
+                console.log(res.data)
+            })
+}, [])
 
   return (
     <Card sx={{ maxWidth: 345 }} className="Card">
